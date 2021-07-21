a1a1647c2f62d9635aaa704b2cc94f7ed138be11
Utitofon-Udoekong <mosesrahul37@gmail.com>
2021-07-22 00:25:58 +0100

    added media queries

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.addEventListener("mousemove", (e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    // spaceBetween: 50,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
