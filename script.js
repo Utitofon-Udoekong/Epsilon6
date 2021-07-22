38765c7a3702ba79590914ed8839a54b76401e85
Utitofon-Udoekong <mosesrahul37@gmail.com>
2021-07-22 23:45:06 +0100

    fixing nav issues

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const cursor = document.querySelector(".cursor")
-document.addEventListener("mousemove", (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
+const navTags = document.querySelectorAll(".nav-wrapper ul li a");
+const navWrapper = document.querySelector(".nav-wrapper");
+const chechBox = document.getElementById("nav")
+navTags.forEach(tag => {
+    tag.addEventListener("click", () => {
+        console.log(chechBox)
+    })
 })
+console.log(navTags);
+console.log(chechBox)
+const nam = -1
+console.log(Number.isInteger(nam))
+
+
+// const cursor = document.querySelector(".cursor")
+// document.addEventListener("mousemove", (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+// })
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
