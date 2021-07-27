3b6a30e9885649525a2ae980b5673d2c7acaebad
Utitofon-Udoekong <mosesrahul37@gmail.com>
2021-07-27 21:59:04 +0100

    added email service

--- contact_script.js
+++ contact_script.js
@@ -1,18 +1,17 @@
-window.onload = function () {
+window.onload = function() {
     emailjs.init("user_nPxcTVCOpmiJntdhkfeU2");
-    
-    document.getElementById('contact-form').addEventListener('submit', (event) => {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm('Contact_Service', 'contact_form', this)
-        .then(function () {
-            console.log('SUCCESS!');
-            modal.classList.add("active")
-        }, function (error) {
-            console.log('FAILED...', error);
-        });
+            .then(function() {
+                console.log('SUCCESS!');
+                modal.classList.add("active")
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
     });
 }
 
