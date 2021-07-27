9d124c8dbb5d66522b96e75865a9c9f7250e7d73
Utitofon-Udoekong <mosesrahul37@gmail.com>
2021-07-27 21:44:08 +0100

    added email service

--- contact_script.js
+++ contact_script.js
@@ -3,12 +3,12 @@ const modal = document.querySelector(".modal")
 window.onload = function () {
     emailjs.init("user_nPxcTVCOpmiJntdhkfeU2");
 
-    document.getElementById('contact-form').addEventListener('submit', async (event) => {
+    document.getElementById('contact-form').addEventListener('submit', (event) => {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
-        await emailjs.sendForm('Contact_Service', 'contact_form', this)
+        emailjs.sendForm('Contact_Service', 'contact_form', this)
             .then(function () {
                 console.log('SUCCESS!');
                 modal.classList.add("active")
